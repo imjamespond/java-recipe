5d3c8b021da1f45ed44f2722af8100770abdf0f9
james <imjamespond@gmai.com>
2017-09-29 22:12:41 +0800

    test ok

--- web-app/src/test/java/PsqlTests.java
+++ web-app/src/test/java/PsqlTests.java
@@ -8,10 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.metasoft.boot.AppServletInitializer;
 import com.metasoft.service.dao.FoobarDaoService;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = AppServletInitializer.class)
 public class PsqlTests {
 	@Autowired
 	FoobarDaoService fs;
