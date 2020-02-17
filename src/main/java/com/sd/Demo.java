b2791da25c273207b80a520f96af70787b08e247
james <imjamespond@gmail.com>
2020-02-17 21:50:23 +0800

    swagger

--- src/main/java/com/sd/Demo.java
+++ src/main/java/com/sd/Demo.java
@@ -8,11 +8,14 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.web.bind.annotation.*;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Collection;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
+@EnableSwagger2
 public class Demo {
 	
 	@GetMapping(path="/user/getName")
@@ -24,8 +27,37 @@ public class Demo {
 		return name;
 	}
 
+	@GetMapping(path="/user/list")
+	@PreAuthorize("hasAuthority('ROLE_ADMIN')")
+	public List<Foobar> list(@RequestParam(value="name", defaultValue="no name") String name ) throws JsonProcessingException {
+		return null;
+	}
+
+	//param: remember-me true
+
 	public static void main(String[] args) {
 		SpringApplication.run(Demo.class, args);
 	}
 	
+}
+
+class Foobar {
+	String foo;
+	int bar;
+
+	public String getFoo() {
+		return foo;
+	}
+
+	public void setFoo(String foo) {
+		this.foo = foo;
+	}
+
+	public int getBar() {
+		return bar;
+	}
+
+	public void setBar(int bar) {
+		this.bar = bar;
+	}
 }
