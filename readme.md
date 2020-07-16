7246144a9498c36ea19399f8123cdc792f090007
James <james@JamesdeMacBook-Pro.local>
2020-07-16 17:35:06 +0800

    add k8s

--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 DOCKER_HOST=unix:///var/run/docker.sock mvn clean install docker:build  
-DOCKER_HOST=tcp://192.168.0.254:2375 mvn clean install docker:build -X  
+DOCKER_HOST=tcp://192.168.0.193:2375 mvn clean install docker:build -X  
 docker tag mavendemo 192.168.0.193:59999/mvndemo  
 docker login  192.168.0.193:59999 -u abc -p abc  
 docker push 192.168.0.193:59999/mvndemo  
--- src/main/java/com/test/Hello.java
+++ src/main/java/com/test/Hello.java
@@ -5,16 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.UUID;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @SpringBootApplication
 @RestController
 public class Hello {
-    static String uuid = UUID.randomUUID().toString();
+    //static String uuid = UUID.randomUUID().toString();
 
     @RequestMapping("/")
-    public String home() {
-        return "Hello Docker World, "+ uuid;
+    public String home() throws UnknownHostException {
+        return "Hello Docker World, from: "+ InetAddress.getLocalHost().getHostName();
     }
 
     public static void main(String[] args) {
