0c12799fbe6f0a3af5b6e9dc80020ef506256707
James <james@JamesdeMacBook-Pro.local>
2020-08-20 14:40:24 +0800

    session,auth,static...

---
+++ src/main/java/com/example/starter/MainVerticle.java
@@ -0,0 +1,90 @@
+package com.example.starter;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.ext.web.handler.SessionHandler;
+import io.vertx.ext.web.handler.StaticHandler;
+import io.vertx.ext.web.sstore.LocalSessionStore;
+import io.vertx.ext.web.sstore.SessionStore;
+
+public class MainVerticle extends AbstractVerticle {
+
+  public static void main(String[] args) {
+    String verticleID = MainVerticle.class.getName();
+    VertxOptions options = new VertxOptions();
+
+    Vertx vertx = Vertx.vertx(options);
+    vertx.deployVerticle(verticleID);
+  }
+
+  @Override
+  public void start(Promise<Void> startPromise) throws Exception {
+
+    final Router router = Router.router(vertx);
+
+    // Set a static server to serve static resources, e.g. the login page
+    router.route(HttpMethod.GET, "/static/*").order(1).handler(StaticHandler.create());
+    // The BodyHandler allows you to retrieve request bodies, limit body sizes and handle file uploads.
+    router.route().order(0).handler(BodyHandler.create().setBodyLimit(1024l));
+
+    SessionStore store = LocalSessionStore.create(vertx);
+    SessionHandler sessionHandler = SessionHandler
+      .create(store)
+      .setSessionTimeout(50000l);//5 seconds
+    // Make sure all requests are routed through the session handler too
+    router.route().handler(sessionHandler);
+
+    router.route("/session/*")/*.consumes("application/json")*/
+      .produces("application/json")
+      .handler(com.example.starter.SessionHandler.create(vertx, router));
+
+    router.route().failureHandler(frc -> {
+      int statusCode = frc.statusCode();
+      Throwable failure = frc.failure();
+      // Status code will be 500 for the RuntimeException, or 403 for the other failure
+      frc.response().setStatusCode(statusCode).end(failure == null ? "Sorry! Not today": failure.getMessage());
+    });
+
+    router.get("/hello/:productID").handler(ctx -> {
+      String productID = ctx.request().getParam("productID");
+      System.out.printf("productID: %s\n", productID);
+        ctx.response().setChunked(true).write("Hello World");
+        ctx.next();
+      }
+    ).blockingHandler(ctx -> {
+      sleep(3000l);
+      ctx.response().putHeader("content-type", "text/html").end();
+    });
+
+
+    vertx
+      .createHttpServer()
+      .requestHandler(router)
+//      .requestHandler(req -> {
+//        req.response()
+//          .putHeader("content-type", "text/plain")
+//          .end("Hello from Vert.x!");
+//      })
+      .listen(8888, http -> {
+        if (http.succeeded()) {
+          startPromise.complete();
+          System.out.println("HTTP server started on port 8888");
+        } else {
+          startPromise.fail(http.cause());
+        }
+      });
+  }
+
+  void sleep(long millis) {
+    try {
+      Thread.sleep(millis);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+}
---
+++ src/main/java/com/example/starter/SessionHandler.java
@@ -0,0 +1,137 @@
+package com.example.starter;
+
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Future;
+import io.vertx.core.Handler;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.auth.AbstractUser;
+import io.vertx.ext.auth.AuthProvider;
+import io.vertx.ext.auth.User;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.Session;
+import io.vertx.ext.web.handler.impl.AuthHandlerImpl;
+import io.vertx.ext.web.handler.impl.HttpStatusException;
+
+public interface SessionHandler extends Handler<RoutingContext> {
+  static SessionHandler create(Vertx vertx, Router router) {
+    return new SessionHandlerImpl(vertx, router);
+  }
+}
+
+class SessionHandlerImpl implements SessionHandler{
+
+  private final Router router;
+  private final AuthProvider authProvider;
+
+  public SessionHandlerImpl(Vertx vertx, Router router) {
+    this.router = router;
+    this.authProvider = new DummyAuthProvider();
+
+    router.route("/get").handler(new DummyAuthHandler(authProvider).addAuthority("role:admin"));
+    router.get("/get").handler(this::getSession);
+    router.get("/set").handler(this::setSession);
+  }
+
+  private void setSession(RoutingContext ctx) {
+    String userName = ctx.request().getParam("username");
+    String password = ctx.request().getParam("password");
+    System.out.printf("username: %s, password: %s\n", userName, password);
+    JsonObject authInfo = new JsonObject()
+      .put("username", userName)
+      .put("password", password);
+    Session session = ctx.session();
+
+    authProvider.authenticate(authInfo,res->{
+      if (res.succeeded()) {
+        session.put("authInfo",authInfo);
+        ctx.response().end("OK");
+      } else {
+        ctx.fail(403, res.cause());
+      }
+    });
+
+  }
+
+  private void getSession(RoutingContext ctx) {
+    Session session = ctx.session();
+    Object user = session.get("authInfo");
+    if(user == null) {
+      ctx.fail(403, new Throwable("user info is null"));
+    }else {
+      ctx.response().end(user.toString());
+    }
+  }
+
+  @Override
+  public void handle(RoutingContext ctx) {
+    router.handleContext(ctx);
+  }
+
+}
+
+class DummyAuthProvider implements AuthProvider {
+
+  private String storedUsername = "foobar";
+  private String storedPassword = "pwd";
+
+  @Override
+  public void authenticate(JsonObject authInfo, Handler<AsyncResult<User>> handler) {
+    if (storedUsername == null || storedPassword == null) {
+      handler.handle(Future.failedFuture("Credentials not set in configuration."));
+      return;
+    }
+
+    String username = authInfo.getString("username");
+    String password = authInfo.getString("password");
+
+    if (storedUsername.equals(username) && storedPassword.equals(password)) {
+      handler.handle(Future.succeededFuture(new UserImpl()));
+    } else {
+      handler.handle(Future.failedFuture("No such user, or password incorrect."));
+    }
+  }
+}
+
+class DummyAuthHandler extends AuthHandlerImpl {
+  public DummyAuthHandler(AuthProvider authProvider) {
+    super(authProvider);
+  }
+
+  @Override
+  public void parseCredentials(RoutingContext ctx, Handler<AsyncResult<JsonObject>> handler) {
+    Session session = ctx.session();
+    JsonObject authInfo = session.get("authInfo");
+    if (session != null && authInfo != null) {
+      ctx.setUser(new UserImpl() ); // avoid auth provider authenticate
+      handler.handle(Future.succeededFuture(authInfo));
+    } else {
+      handler.handle(Future.failedFuture(new HttpStatusException(401) ));
+      // Now redirect to the login url - we'll get redirected back here after successful login
+      //handler.handle(Future.failedFuture(new HttpStatusException(302, "/hello")));
+    }
+  }
+}
+
+class UserImpl extends AbstractUser {
+
+//  private JsonObject authInfo;
+
+  @Override
+  protected void doIsPermitted(String permission, Handler<AsyncResult<Boolean>> resultHandler) {
+    System.out.println(permission);
+    resultHandler.handle(Future.succeededFuture(true));
+  }
+
+  @Override
+  public JsonObject principal() {
+    System.out.println("principal");
+    return null;
+  }
+
+  @Override
+  public void setAuthProvider(AuthProvider authProvider) {
+    System.out.println("setAuthProvider");
+  }
+}
